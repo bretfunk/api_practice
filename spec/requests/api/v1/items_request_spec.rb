171d870ae14976682ed3f325e33531105572b1e8
BFunk <bret.e.funk@gmail.com>
2017-08-12 18:04:02 -0600

    added show api

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -5,10 +5,17 @@ describe "Items API" do
     create_list(:item, 3)
 
     get '/api/v1/items'
-
     expect(response).to be_success
 
     items = JSON.parse(response.body)
+
+    expect(items.count).to eq(3)
+  end
+
+  it "can get one item by its id" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
   end
 end
 
