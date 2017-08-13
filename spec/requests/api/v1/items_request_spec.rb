962efe0380b2b58114bdd7791e5c6469063f71d6
BFunk <bret.e.funk@gmail.com>
2017-08-12 18:08:46 -0600

    added create

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -16,6 +16,23 @@ describe "Items API" do
     id = create(:item).id
 
     get "/api/v1/items/#{id}"
+
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(item["id"]).to eq(id)
+  end
+
+  it "can create a new item" do
+    item_params = { name: "Saw", description: "I want to play a game" }
+
+    post "/api/v1/items", params: {item: item_params}
+    item = Item.last
+
+    assert_response :success
+    expect(response).to be_success
+    expect(item.name).to eq(item_params[:name])
   end
 end
 
